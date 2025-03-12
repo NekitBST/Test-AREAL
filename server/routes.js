3fda319507c2dfaddb9278c23f514e9a41736b2f
Никита <nikata.shatalov.04@mail.ru>
2025-03-13 01:16:25 +0300

    add the method "add employee"

--- server/routes.js
+++ server/routes.js
@@ -10,7 +10,21 @@ router.get('/employees', async (req, res) => {
         res.json(result.rows);
     } catch (err) {
         console.error('Ошибка при получении полного списка сотрудников (не уволенных):', err);
-        res.status(500);
+        res.sendStatus(500);
+    }
+});
+
+router.post('/employees', async (req, res) => {
+    const { full_name, birth_date, passport, contact, address, department, position, salary, hire_date } = req.body;
+    try {
+        const result = await pool.query(
+            'INSERT INTO employees (full_name, birth_date, passport, contact, address, department, position, salary, hire_date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *',
+            [full_name, birth_date, passport, contact, address, department, position, salary, hire_date]
+        );
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error('Ошибка при создании сотрудника:', err);
+        res.sendStatus(500);
     }
 });
 
--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use('/api', routes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Сервер запущен на порту ${PORT}`);
 });
